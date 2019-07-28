bfff5ecb108907e27815ec27ac62f211b7a955bd
Divyanshu <divyanshu.r46956@gmail.com>
2019-07-28 21:20:51 +0200

    Added shell fn to execute scripts, patterns 🎧

--- fileExtension.sh
+++ fileExtension.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
 # Change filename extensions
+# Alternative Way !
+# [[ $# -ne 2 ]] && { echo "Need exactly two arguments" >&2; exit 1; }
 
+# for f in *"$1"; do
+# 	base=$(basename "$f" "$1")
+# 	mv "$f" "${base}$2"
+# done
+
+
+
+#!/bin/bash
+
+# Change filename extensions
+# Pattern Matching!
 [[ $# -ne 2 ]] && { echo "Need exactly two arguments" >&2; exit 1; }
 
 for f in *"$1"; do
-	base=$(basename "$f" "$1")
-	mv "$f" "${base}$2"
+	mv -- "$f" "${f/%$1/$2}"
 done
 
